bbb635567b54980a69df72ff766524a4794e26b8
Jack Waghan <jackwaghan@gmail.com>
2025-08-04 10:30:17 +0530

    Added Temporary home route with redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-"use client";
-
-import { useTheme } from "next-themes";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const { setTheme, themes } = useTheme();
-  if (typeof window === "undefined") {
-    return null;
-  }
-  return (
-    <div className="flex flex-col gap-4">
-      {themes.map((theme) => (
-        <button key={theme} onClick={() => setTheme(theme)}>
-          {theme}
-        </button>
-      ))}
-    </div>
-  );
+  return redirect("/auth/login");
 }
