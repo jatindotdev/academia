2bdc1216a1c030b2a4446e700951cd0e5cb60868
Jack Waghan <jackwaghan@gmail.com>
2025-08-12 19:26:19 +0530

    Integrated PWA

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+export default withPWA({
   experimental: {
     globalNotFound: true,
   },
-};
-
-export default nextConfig;
+});
